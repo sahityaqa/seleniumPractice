c63573396b9f4d489db6bae1cc4be069369cddcb
sahityaqa <bitra.sahitya@gmail.com>
2022-10-22 08:00:11 +0530

    Add practice

---
+++ PracticeProject1/src/com/src/package1/Class11.java
@@ -0,0 +1,21 @@
+package com.src.package1;
+
+import com.src.practice.Class1;
+
+public class Class11 extends Class1 {
+
+	public Class11(int a, int b) {
+		super(a, b);
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Class11 c11 = new Class11(10,5);
+		c11.Add();
+		c11.Sub();
+		
+
+	}
+
+}
